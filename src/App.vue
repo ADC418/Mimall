83b816409be20c5fc8721641687bf2096f06db62
吴笛 <1632737798@qq.com>
2020-08-19 23:02:59 +0800

    结构

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <router-view></router-view>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import axios from "axios";
-//import jsonp from "jsonp"
+//import axios from "axios";
+import jsonp from "jsonp"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
   },
   data(){
     return{
       age:30,
-      data:""
+     
     }
   },
   mounted(){
-    //axios是在network中的XHR中查看
-    // axios({
-    //   url:"https://cnodejs.org/api/v1/topics",
-    //   method:"get"
-    // }).then(res=>{
-    //   console.log(res)
-    // }).catch(err=>{
-    //   console.log(err)
-    // })
-    
-    // jsonp(url,(err,res)=>{
-    //   //jsonp是在network中的js查看
-    //   let result=res;
-    //   this.data=result;
-    // })
-    axios({
-      url:"/api/v1/topics",
-      method:"get",
-    }).then(res=>{
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
-    })
-  }
+   
+   }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    devServer: {
+    devServer: { //名字固定
         host: "localhost",
-        port: 8080,
-        proxy: {
-            "/api": {
-                target: "https://cnodejs.org",
-                changeOrigin: true,
+        port: 8080, 
+        proxy: { //代理
+            "/api": { //替换代理地址名称 拦截到/api 访问下面的地址 访问a，实际访问的是b
+                target: "https://cnodejs.org", //代理目标
+                changeOrigin: true,//可否跨域
                 pathRewrite: {
-                    "/api":"/api"
+                    "/api": "/api",//重写接口
                 }
             }
         }
