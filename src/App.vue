a8b5da58ff138fa2443a528c4ce77080a2cc8821
吴笛 <1632737798@qq.com>
2020-09-05 10:17:37 +0800

    json请求 实现mock

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,12 @@ export default {
     return {};
   },
   mounted() {
-    // storage.setItem("mall",{"user":{"a":"1","age":18,"sex":1}})
-    // storage.setItem("a",1)
-    // storage.setItem("user",{"b":1})
-    // storage.clear("a");
-    // storage.clear("a", "user"); //删除user下的a
+    //方式1 通过本地加载静态json文件的形式实现数据mock ，需要修改代码-baseURL
+    //public/ 是根目录，不用写
+    this.axios.get("/mock/user/login.json").then((res)=>{
+        this.res=res;//
+    })
+
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import routers from "./router"
 
 import axios from "axios"
-import VueAxios from "vue-axios"
-import env from  "./env"
+import VueAxios from "vue-axios" //调用时通过this.axios.xxx即可
+//import env from  "./env"
 import App from './App.vue'
 
 //根据前端的跨域方法做调整  jsonp或cors代理 eg接口写成http:"//www.baidu.com" 不同的跨域方式，配置不同
-axios.defaults.baseURL="/api";//用接口代理的方式 当前接口的域名和前端一样 习惯写成 /api 
+axios.defaults.baseURL="/";//用接口代理的方式 当前接口的域名和前端一样 习惯写成 /api 
 //访问 /a/b : /api/a/b 但在转发时会自动把/api去掉 => /a/b
 axios.defaults.timeout=8000;//超时时间 提高用户体验
 //根据环境变量获取不同的请求地址
-axios.defaults.baseURL=env.baseURL
+//axios.defaults.baseURL=env.baseURL
 //inter 拦截器  接口错误拦截
 /* 
 接口规范
