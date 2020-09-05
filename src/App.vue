eb39633e77415bfdeb205452a48d07578fc44dc5
吴笛 <1632737798@qq.com>
2020-09-05 11:30:26 +0800

    mock设置数据

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,19 @@ export default {
   },
   mounted() {
     //方式1 通过本地加载静态json文件的形式实现数据mock ，需要修改代码-baseURL
-    //public/ 是根目录，不用写
-    this.axios.get("/mock/user/login.json").then((res)=>{
-        this.res=res;//
-    })
-
+    // public/ 是根目录，不用写
+    // this.axios.get("/mock/user/login.json").then((res)=>{
+    //     this.res=res;//修改baseURL地址 axios.defaults.baseURL=""
+    // })
+    //方式2 通过easy-mock平台实现数据mock
+    // this.axios.get("/topics").then(res => {
+    //   this.res = res; //也需要修改代码-baseURL 写成接口地址axios.defaults.baseURL="https://cnodejs.org/api/v1"
+    // });
+    //方式3 本地集成mockjs 实现数据mock 需要定义一个开关 
+     this.axios.get("/user/login").then(res => {
+      this.res = res; 
+      console.log(this.res)
+    });
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import VueAxios from "vue-axios" //调用时通过this.axios.xxx即可
 //import env from  "./env"
 import App from './App.vue'
 
+const mock=true;//定义一个开关
+if(mock){
+  require('./mock/api');//因为import是预编译加载,编译的时候，import加载的文件就会执行  require不是从上到下执行的时候加载
+}
 //根据前端的跨域方法做调整  jsonp或cors代理 eg接口写成http:"//www.baidu.com" 不同的跨域方式，配置不同
-axios.defaults.baseURL="/";//用接口代理的方式 当前接口的域名和前端一样 习惯写成 /api 
+axios.defaults.baseURL="/api";//用接口代理的方式 当前接口的域名和前端一样 习惯写成 /api 
 //访问 /a/b : /api/a/b 但在转发时会自动把/api去掉 => /a/b
 axios.defaults.timeout=8000;//超时时间 提高用户体验
 //根据环境变量获取不同的请求地址
